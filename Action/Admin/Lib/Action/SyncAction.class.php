da6ce766d3e8518e22f236742128157619ee816a
yuzq <393008735@qq.com>
2020-04-16 19:16:26 +0800

    修改

--- Action/Admin/Lib/Action/SyncAction.class.php
+++ Action/Admin/Lib/Action/SyncAction.class.php
@@ -151,7 +151,7 @@ class SyncAction extends CommonAction
             array('厦门智赢', 'http://erp.travel.pwgci.com/'),
             array('山西红马', 'http://hongma.serp.cncn.net/'),
             array('成都铁路', 'http://cdtl.d12.cncn.net/'),
-            array('福建超越未来', 'http://cywl.serp.cncn.net/'),
+            array('福建超越未来', 'http://erp.cywlly.com/'),
             array('广东中旅', 'http://gdzl.serp.cncn.net/'),
 		);
 		
