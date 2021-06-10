523074fe6daca4266aedfc83fcd83151825332ba
yuzq <393008735@qq.com>
2021-06-10 15:46:28 +0800

    修改

--- Action/Admin/Lib/Action/SyncAction.class.php
+++ Action/Admin/Lib/Action/SyncAction.class.php
@@ -157,6 +157,7 @@ class SyncAction extends CommonAction
             array('福建超越未来', 'http://erp.cywlly.com/'),
             array('广东中旅', 'http://gdzl.serp.cncn.net/'),
             array('安徽中青', 'http://erp.cytsah.com/'),
+            array('国旅环球', 'http://erp.citsglobe.com/'),
 		);
 		
 		$this->assign('server_list', $server_list);
